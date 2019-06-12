a8ff79fc1b3f30699372b054e08307ef62b3c394
silingxy <1095447583@qq.com>
2019-06-12 17:30:09 +0800

    commit

--- src/main/java/com/CTCC/CRBT/Controller/AccountController.java
+++ src/main/java/com/CTCC/CRBT/Controller/AccountController.java
@@ -3,6 +3,7 @@ package com.CTCC.CRBT.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.CTCC.CRBT.DAO.PageResults;
